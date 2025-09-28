064ad3cd346256c03515638287dfcc86511163fc
mprusac <mprusac0@gmail.com>
2025-09-28 23:51:02 +0200

    Integriran search s backendom, prikaz temperature/vlage/vjetra i dodan error handling

--- script.js
+++ script.js
@@ -1,18 +1,68 @@
-import dotenv from "dotenv";
-dotenv.config();
+
+const apiUrl = "http://localhost:3000/weather"
+
+const searchBox = document.querySelector(".search input");
+const searchBtn = document.querySelector(".search button");
+const weatherIcon = document.querySelector(".weather-icon");
+
+async function checkWeather(city) { //asinkrona funkcija - radi stvari koje mogu trajati duze, npr cekanje odgovora sa servera - bez blokiranja ostatka programa
+  try {
+    const response = await fetch(`${apiUrl}?city=${city}`); //u asnyc funkciji se moze koristiti samo kljucna rijec await
+    const data = await response.json();
+
+    console.log("DEBUG", data);
 
 
-const apiUrl = "https://api.openweathermap.org/data/2.5/weather?&units=metric&q=busovaca";
 
-async function checkWeather() { //asinkrona funkcija - radi stvari koje mogu trajati duze, npr cekanje odgovora sa servera - bez blokiranja ostatka programa
-  const response = await fetch(apiUrl + `&appid=${apiKey}`); //u asnyc funkciji se moze koristiti samo kljucna rijec await
-  var data = await response.json(); //fetch - ugradjena funkcija koja sluzi za slanje HTTP zahtjeva - GET, POST..., ovdje saljem zahtjev na apiUrl tj OpenWeatherMap API i dodajem API kljuc
-  //sto je potrebno da bi API pustio podatke, await - ceka da se fetch izvrsi tj dok server odgovori
-  //bez await, response bi bio samo obecanje tj promise, a sa await response sadrzi stvarni odgovor
+    if (data.cod === "404") {
+      document.querySelector(".error").style.display = "block";
+      document.querySelector(".weather").style.display = "block";
+      return;
+    }
 
-  //data - unutar varijablr response.json() pretvara odgovor sa servera u JSON objekt, citljiv za JS, i opet await jer traje neko vrijeme
 
-  console.log(data);
+
+    //fetch - ugradjena funkcija koja sluzi za slanje HTTP zahtjeva - GET, POST..., ovdje saljem zahtjev na apiUrl tj OpenWeatherMap API i dodajem API kljuc
+    //sto je potrebno da bi API pustio podatke, await - ceka da se fetch izvrsi tj dok server odgovori
+    //bez await, response bi bio samo obecanje tj promise, a sa await response sadrzi stvarni odgovor
+
+    //data - unutar varijablr response.json() pretvara odgovor sa servera u JSON objekt, citljiv za JS, i opet await jer traje neko vrijeme
+
+    document.querySelector(".city").innerHTML = data.name;
+    document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + " °C";
+    document.querySelector(".humidity").innerHTML = data.main.humidity + " %";
+    document.querySelector(".wind").innerHTML = Math.round(data.wind.speed) + " km/h";
+
+    const icons = {
+      Clouds: "images/clouds.png",
+      Clear: "images/clear.png",
+      Rain: "images/rain.png",
+      Drizzle: "images/drizzle.png",
+      Mist: "images/mist.png",
+    };
+
+    weatherIcon.src = icons[data.weather[0].main] || "images/default.png";
+
+    document.querySelector(".error").style.display = "none";
+    document.querySelector(".weather").style.display = "block";
+
+  } catch (error) {
+    console.log("Greška pri dohvatu podataka: ", error)
+  }
 }
 
-checkWeather(); 
+
+document.querySelector(".error").style.display = "none";
+
+
+// Sakrij error kad kreneš tipkat
+searchBox.addEventListener("input", () => {
+  document.querySelector(".error").style.display = "none";
+});
+
+// Klik na search dugme
+searchBtn.addEventListener("click", () => {
+  document.querySelector(".error").style.display = "none"; // reset error
+  checkWeather(searchBox.value);
+});
+
